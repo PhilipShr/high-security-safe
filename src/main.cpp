18e697031a5c53cb26e41f8178c60b37ea8f9500
PhilipShr <schwengsbier.philip@rdf.nuernberg.de>
2024-06-04 10:00:45 +0000

    add state_machine

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,78 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// States definieren
+typedef enum
+{
+  safe_locked,
+  level1_unlocked,
+  level2_unlocked,
+  level3_unlocked,
+  safe_open
+} State_t;
+// transition events definieren können öfter verwendet werden
+typedef enum
+{
+  input1_accepted,
+  input2_accepted,
+  input3_accepted,
+  open_door,
+  close_door,
+  input_refused
+} Event_t;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void state_machine(Event_t event);
+State_t state = safe_locked;
+
+// Statemachine
+void state_machine(Event_t event)
+{
+  switch (state)
+  {
+  case safe_locked:
+    if (input1_accepted == event)
+    {
+      state = level1_unlocked;
+    }
+
+    break;
+  case level1_unlocked:
+    if (input2_accepted == event)
+    {
+      state = level2_unlocked;
+    }
+    else if (input_refused == event)
+    {
+      state = safe_locked;
+    }
+    break;
+  case level2_unlocked:
+    if (input3_accepted == event)
+    {
+      state = level3_unlocked;
+    }
+    else if (input_refused == event)
+    {
+      state = safe_locked;
+    }
+    break;
+    // Zustand in dem ich mich befinde
+  case level3_unlocked:
+    // Wie ich in einen anderen Zustand komme
+    if (open_door == event)
+    {
+      // in welchen Zustand ich kommen will
+      state = safe_open;
+    }
+    break;
+  case safe_open:
+    if (close_door == event)
+    {
+      state = safe_locked;
+    }
+    break;
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void setup();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+void loop();
